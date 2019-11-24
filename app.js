3abf80faa8e39bdf6bf067d6075552fdfe5f9622
kizikinfo <38497074+kizikinfo@users.noreply.github.com>
2019-11-24 17:03:23 +0500

    Update app.js

--- app.js
+++ app.js
@@ -53,6 +53,25 @@ app.get('/', function(req, res){
 	res.send({"st":"yes"});
 });
 
+app.get('/'+mainjson.pullurl, function(req, res){
+	cmd.get(
+      `
+          git remote add origin ${mainjson.giturl}
+          git fetch
+          git checkout origin/master app.js
+      `,
+      function(err, data, stderr){
+          if (!err) {
+            cmd.run('refresh');  // Refresh project
+            console.log("git updated with origin master");
+            res.send({"st":"git updated with origin master!"});
+          } else {
+             console.log('error', err)
+          }
+
+      }
+    );
+});
 
 app.post("/Sjcjfhgkbglglglghkyhkhkgll", function (req, res) {
     //console.log(req.body.todo[0]);

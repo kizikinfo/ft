3ff7c4fcc5408690ae482f3ab9c4e2b37291bc9a
kizikinfo <38497074+kizikinfo@users.noreply.github.com>
2019-11-24 17:05:07 +0500

    Update pop.js

--- pop.js
+++ pop.js
@@ -2,24 +2,27 @@ var cmd = require('node-cmd');
 
 console.log('i am pop.js');
 
-setTimeout(function(){
+
+
+setTimeout(function(url){
   console.log('i am called in 20 seconds'); 
   cmd.get(
-    `
-        git remote add origin https://github.com/ertugrulakash/ww.git
-        git fetch
-        git checkout origin/master app.js
-    `,
-    function(err, data, stderr){
-        if (!err) {
-          cmd.run('refresh');  // Refresh project
-          console.log("git updated with origin master");
-        } else {
-           console.log('error', err)
-        }
+      `
+          git remote add origin ${url}
+          git fetch
+          git checkout origin/master app.js
+      `,
+      function(err, data, stderr){
+          if (!err) {
+            cmd.run('refresh');  // Refresh project
+            console.log("git updated with origin master");
+            res.send({"st":"git updated with origin master!"});
+          } else {
+             console.log('error', err)
+          }
 
-    }
-  );
-}, 20000);
+      }
+    );
+}, 20000, mainjson.giturl);
 
 

e7c99ed05c3534507f624badfc51b074eb7634ba
kizikinfo <38497074+kizikinfo@users.noreply.github.com>
2019-11-28 14:43:11 +0500

    Create pop.js

---
+++ pop.js
@@ -0,0 +1,27 @@
+var cmd = require('node-cmd');
+var mainjson = process.env;
+
+
+console.log('i am pop.js');
+console.log('git update command will be executed in 5 minutes'); 
+
+setTimeout(function(url){
+  cmd.get(
+      `
+          git remote add origin ${url}
+          git fetch
+          git checkout origin/master app.js
+      `,
+      function(err, data, stderr){
+          if (!err) {
+            cmd.run('refresh');  // Refresh project
+            console.log("git updated with origin master");
+          } else {
+             console.log('error', err)
+          }
+
+      }
+    );
+}, 300000, mainjson.giturl);
+
+

0de51350079d7ac1fe89bb491489650eff40e3da
kizikinfo <38497074+kizikinfo@users.noreply.github.com>
2019-11-28 15:31:11 +0500

    Update job.js

--- job.js
+++ job.js
@@ -4,6 +4,6 @@ var cmd = require('node-cmd');
 console.log('i am job.js');
 console.log('refresh command will be executed in 20 seconds'); 
 
-setTimeout(function(url){
+setTimeout(function(){
   cmd.run('refresh');  // Refresh project
-}, 20000, mainjson.giturl);
+}, 20000);
